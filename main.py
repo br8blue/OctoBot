7c5225ae90e0c52d0dc7a7eb48e5b04897c55941
Praneel Srinivasan <neelsrini2012@gmail.com>
2025-09-21 14:42:24 -0400

    Switching to Mythomax

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from gpt4all import GPT4All
 import os
+import flask
+
+model_path = r"C:\Users\neels_xc\AILocal\mythomax-l2-13b.Q4_K_M.gguf"
+model_file = "mythomax-l2-13b.Q4_K_M.gguf"
+full_path = r"C:\Users\neels_xc\AILocal\mythomax-l2-13b.Q4_K_M.gguf"
+
+model = GPT4All(r"C:\Users\neels_xc\AILocal\mythomax-l2-13b.Q4_K_M.gguf")
 
 
-model_path = r"C:\Users\neels_xc\AILocal\mistral-7b-instruct-v0.2.Q6_K.gguf"
-model_file = "mistral-7b-instruct-v0.2.Q6_K.gguf"
-full_path = r"C:\Users\neels_xc\AILocal\mistral-7b-instruct-v0.2.Q6_K.gguf"
 
 
 if not os.path.exists(full_path):
     raise FileNotFoundError(f"Cannot find model file at: {full_path}")
 
-
-model = GPT4All(r"C:\Users\neels_xc\AILocal\mistral-7b-instruct-v0.2.Q6_K.gguf")
 
 
 print("OctoBot: Hello there! My name is Octobot. How may I help you?")
