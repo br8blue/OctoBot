d7c7e7ebc6ac3f8a7d29370e2d44166cdd813ed7
Praneel Srinivasan <neelsrini2012@gmail.com>
2025-09-28 21:00:35 -0400

    JS Updates

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ sendButton.onclick = async () => {
 
 
 try {
-  const res = await fetch("http://127.0.0.1:5000", {
+  const res = await fetch("http://127.0.0.1:5000/chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message })
-  });
+});
+
 
   if (!res.ok) {
     throw new Error(`HTTP error ${res.status}`);
